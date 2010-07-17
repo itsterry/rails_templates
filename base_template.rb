24b58e799f0abd85aabd3abaeb9c176033691bc0
Terry S <itsterry@gmail.com>
2010-07-17 23:55:59 +0100

    tweaked add submodule

--- base_template.rb
+++ base_template.rb
@@ -64,5 +64,5 @@ git :rm=>"public/index.html"
 git :add => '.'
 git :commit =>"-m 'initial commit'"
 
-run 'git submodule add git@github.com:itsterry/rails_libraries.git lib'
+run 'git submodule add git@github.com:itsterry/rails_libraries.git lib/rails_libraries'
 
