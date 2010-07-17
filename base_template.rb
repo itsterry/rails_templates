0199d6441d4a7970db353a5d3f7044158a17413f
Terry S <itsterry@gmail.com>
2010-07-17 22:19:01 +0100

    Added a bunch more plugins and gems

--- base_template.rb
+++ base_template.rb
@@ -3,12 +3,26 @@ run "echo TODO > README"
 gem 'haml'
 gem 'prawn'
 gem 'mislav-will_paginate', :lib=>'will_paginate',:source=>'http://gems.github.com'
+gem 'formtastic'
+gem "rmagick", :lib => "RMagick2"
+gem "spreadsheet"
+gem "aws-s3", :lib=>'aws/s3'
+gem "chronic"
+gem 'javan-whenever', :lib=>false, :source => 'http://gems.github.com'
+gem 'ryanb-acts-as-list', :lib => 'acts_as_list', :source => 'http://gems.github.com'
+gem 'delayed_job'
+gem 'calendar_date_select'
+gem 'formtastic_calendar_date_select', :source=>"http://gemcutter.org"
 rake 'gems:install'
 
 plugin :make_resourceful, :svn=>'http://svn.hamptoncatlin.com/make_resourceful/tags/make_resourceful'
 plugin "state_machine", :git=>"git://github.com/pluginaweek/state_machine.git"
 plugin "rspec", :git=>"git://github.com/dchelimsky/rspec.git"
 plugin "rspec-rails", :git=>"git://github.com/dchelimsky/rspec-rails.git"
+plugin :mimetype_fu, :svn=>"http://mimetype-fu.googlecode.com/svn/trunk/"
+plugin :formtastic_calendar_date_select, :git=>"git://github.com/dyohi/formtastic_calendar_date_select"
+plugin :exception_notification
+plugin :attachment_fu, :git=>'git://github.com/technoweenie/attachment_fu.git'
 generate :rspec
 
 #plugin :restful_authentication, :svn=>'http://svn.techno-weenie.net/projects/plugins/restful_authentication'
